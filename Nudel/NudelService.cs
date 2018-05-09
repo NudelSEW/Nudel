09994e25feb023fafb1ab8ab731271b1f9dea427
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-09 11:27:58 +0200

    Implemented Basic NudelService

---
+++ Nudel/NudelService.cs
@@ -0,0 +1,31 @@
+﻿using Nudel.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nudel.Core
+{
+    public class NudelService
+    {
+        private String connectionToken;
+        public User User { get; set; }
+
+
+        public NudelService()
+        {
+
+        }
+
+        public void Login(User user)
+        {
+
+        }
+
+        public void Logout()
+        {
+
+        }
+    }
+}
