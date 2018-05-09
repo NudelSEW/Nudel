e83817fc0e0ea14d4be687cc6ba9b318706dff87
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-09 11:27:49 +0200

    Implemented Basic User Structure

---
+++ Nudel/BusinessObjects/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nudel.BusinessObjects
+{
+    public class User
+    {
+        public long ID { get; set; }
+        string FirstName { get; set; }
+        string SecondName { get; set; }
+        string Email { get; set; }
+    }
+}
