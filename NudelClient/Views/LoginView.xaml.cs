1f52aa832f66314c1270534c308cb1ee729aaa8d
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-11 17:22:06 +0200

    Designed LoginView

--- NudelClient/Views/LoginView.xaml.cs
+++ NudelClient/Views/LoginView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace NudelClient.Views
+namespace Nudel.Client.Views
 {
     /// <summary>
     /// Interaction logic for LoginView.xaml
