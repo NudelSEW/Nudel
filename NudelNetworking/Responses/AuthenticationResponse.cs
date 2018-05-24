88cd6efb8567c8e878f6762500dab5709ea103fb
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 01:17:58 +0200

    Added Networking Response

---
+++ NudelNetworking/Responses/AuthenticationResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Nudel.Networking.Responses
+{
+    public class AuthenticationResponse
+    {
+        public string SessionToken;
+
+        public AuthenticationResponse() { }
+        public AuthenticationResponse(string sessionToken)
+        {
+            SessionToken = sessionToken;
+        }
+    }
+}
