b8eb6787902cafe8fba1693d5c6bd3c7e7ffa3f8
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 01:16:21 +0200

    Implemented basic NudelService

--- NudelBackend/NudelService.cs
+++ NudelBackend/NudelService.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Nudel.BusinessObjects;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nudel.Backend
 {
-    class NudelService
+    public class NudelService
     {
+        public NudelService() => throw new NotImplementedException();
+
+        public void Register(string username, string email, string password, string firstName, string lastName) => throw new NotImplementedException();
+        public string Authenticate(string usernameOrEmail, string password) => throw new NotImplementedException();
+        public void CreateEvent(Event @event) => throw new NotImplementedException();
+        public Event FindEvent(long id) => throw new NotImplementedException();
+        public List<Event> FindEvents(string title) => throw new NotImplementedException();
+        public void SubscribeEvent(Event @event) => throw new NotImplementedException();
+        public User FindUser(long id) => throw new NotImplementedException();
+        public User FindUser(string usernameOrEmail) => throw new NotImplementedException();
+        public void NotifyUser(Event @event, User user) => throw new NotImplementedException();
+        public void NotifyUsers(Event @event, List<User> user) => throw new NotImplementedException();
     }
 }
