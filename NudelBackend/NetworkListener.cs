231d94bbb320988c7a83db0789f979d5419d93f6
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 01:15:28 +0200

    Created NetworkListener

---
+++ NudelBackend/NetworkListener.cs
@@ -0,0 +1,62 @@
+﻿using JustConnect.Tcp;
+using Newtonsoft.Json;
+using Nudel.Networking.Requests;
+using Nudel.Networking.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nudel.Backend
+{
+    public class NetworkListener
+    {
+        private const int PORT = 8181;
+        private Server server;
+        private NudelService nudel;
+        private JsonSerializerSettings jsonSettings;
+
+        public NetworkListener()
+        {
+            server = new Server(PORT);
+            nudel = new NudelService();
+            jsonSettings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            server.Received += ProcessRequest;
+        }
+
+        public void Start()
+        {
+            server.Start();
+        }
+        public void Stop()
+        {
+            server.Stop();
+        }
+        private void ProcessRequest(string data, Socket clientSocket)
+        {
+            Request rawRequest = JsonConvert.DeserializeObject<Request>(data, jsonSettings);
+
+            if (rawRequest is AuthenticationRequest)
+            {
+                AuthenticationRequest request = rawRequest as AuthenticationRequest;
+                string sessionToken = nudel.Authenticate(request.UsernameOrEmail, request.Password);
+
+                string response = JsonConvert.SerializeObject(new AuthenticationResponse(sessionToken));
+
+                server.Send(response, clientSocket);
+            }
+            else if (rawRequest is CreateEventRequest)
+            {
+                CreateEventRequest request = rawRequest as CreateEventRequest;
+                nudel.CreateEvent(request.Event);
+            }
+        }
+    }
+}
--- NudelBackend/Program.cs
+++ NudelBackend/Program.cs
@@ -1,39 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-
-using TcpServer = JustConnect.Tcp.Server;
 
 namespace Nudel.Backend
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            TcpServer server = new TcpServer(8000);
-            NudelService nudel = new NudelService();
-
-            server.Log += (string data) =>
-            {
-                Console.WriteLine($"Log: {data}");
-            };
-            server.Accepted += (Socket clientSocket) =>
-            {
-                Console.WriteLine($"Client accepted: {((IPEndPoint)clientSocket.RemoteEndPoint).Address}");
-            };
-            server.Received += (string data, Socket clientSocket) =>
-            {
-                Console.WriteLine($"{((IPEndPoint)clientSocket.RemoteEndPoint).Address} sent:");
-                Console.WriteLine(data);
-
-                server.Send(data, clientSocket);
-            };
-
-            server.Start();
+            NetworkListener networkListener = new NetworkListener();
+            networkListener.Start();
 
             Console.ReadLine();
         }
