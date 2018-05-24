cf45ac99329f9b1c496e9f3475dd518da9258974
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 01:15:01 +0200

    Implemented User Object

--- NudelBusinessObjects/User.cs
+++ NudelBusinessObjects/User.cs
@@ -1,6 +1,23 @@
+﻿using System.Collections.Generic;
+
 namespace Nudel.BusinessObjects
 {
     public class User
     {
+        public long ID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public List<Event> OwnedEvents { get; set; }
+
+        public User() { }
+        public User(long id, string firstName, string secondName, string email, List<Event> ownedEvents)
+        {
+            ID = id;
+            FirstName = firstName;
+            SecondName = secondName;
+            Email = email;
+            OwnedEvents = ownedEvents;
+        }
     }
 }
