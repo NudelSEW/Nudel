b151ace07bf04ada50134920c28f7a6e599652b1
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 01:14:52 +0200

    Implemented Event Object

--- NudelBusinessObjects/Event.cs
+++ NudelBusinessObjects/Event.cs
@@ -1,6 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
 namespace Nudel.BusinessObjects
 {
     public class Event
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Time { get; set; }
+        public Location Location { get; set; }
+        public User Owner { get; set; }
+        public List<User> Members { get; set; }
+        public List<DateTime> Options { get; set; }
+
+        public Event() { }
+        public Event(
+            string title,
+            string description,
+            DateTime time,
+            Location location,
+            User owner,
+            List<User> members,
+            List<DateTime> options
+        )
+        {
+            Title = title;
+            Description = description;
+            Time = time;
+            Location = location;
+            Owner = owner;
+            Members = members;
+            Options = options;
+        }
     }
 }
