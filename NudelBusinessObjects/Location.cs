346626690627426e10655009e6831a57399946f2
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 01:15:09 +0200

    Implemented Location Object

---
+++ NudelBusinessObjects/Location.cs
@@ -0,0 +1,14 @@
+﻿namespace Nudel.BusinessObjects
+{
+    public struct Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public Location(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
