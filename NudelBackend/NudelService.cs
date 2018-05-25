631949b5b0e77cc17579450480a89b487df07a0e
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 13:51:54 +0200

    NudelService: Added NotImplementedExceptions to functions

--- NudelBackend/NudelService.cs
+++ NudelBackend/NudelService.cs
@@ -20,12 +20,19 @@ namespace Nudel.Backend
 
         public void Register(string username, string email, string password, string firstName, string lastName) => throw new NotImplementedException();
         public string Authenticate(string usernameOrEmail, string password) => throw new NotImplementedException();
-        public void CreateEvent(Event @event) => throw new NotImplementedException();
+
+        public void CreateEvent(string title, string description, DateTime time, Location location, List<DateTime> options) => throw new NotImplementedException();
+
         public Event FindEvent(long id) => throw new NotImplementedException();
         public List<Event> FindEvents(string title) => throw new NotImplementedException();
+
         public void SubscribeEvent(Event @event) => throw new NotImplementedException();
+        public void SubscribeEvent(long id) => throw new NotImplementedException();
+        public void SubscribeEvent(string title) => throw new NotImplementedException();
+
         public User FindUser(long id) => throw new NotImplementedException();
         public User FindUser(string usernameOrEmail) => throw new NotImplementedException();
+
         public void NotifyUser(Event @event, User user) => throw new NotImplementedException();
         public void NotifyUsers(Event @event, List<User> user) => throw new NotImplementedException();
     }
