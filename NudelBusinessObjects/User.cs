3e4c8bc49ecb50bc89285f7c9b7644977d0c333d
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 13:49:19 +0200

    Added Username and Password to User

--- NudelBusinessObjects/User.cs
+++ NudelBusinessObjects/User.cs
@@ -5,18 +5,22 @@ namespace Nudel.BusinessObjects
     public class User
     {
         public long ID { get; set; }
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public List<Event> OwnedEvents { get; set; }
 
         public User() { }
-        public User(long id, string firstName, string secondName, string email, List<Event> ownedEvents)
+        public User(long id, string username, string email, string password, string firstName, string lastName, List<Event> ownedEvents)
         {
             ID = id;
-            FirstName = firstName;
-            SecondName = secondName;
+            Username = username;
             Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
             OwnedEvents = ownedEvents;
         }
     }
