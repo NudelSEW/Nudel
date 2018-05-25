76c1539af0487f427ce1104a3194f4aa5e4434eb
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2018-05-25 22:39:24 +0200

    Added Request and Response Base Types

---
+++ NudelNetworking/Responses/Base/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nudel.Networking.Responses.Base
+{
+    public class Response
+    {
+        public Response() { }
+    }
+}
