3337638d83be50e654df70176474800e9935e808
ovicovac <ovicovac@student.tgm.ac.at>
2018-06-07 01:18:06 +0200

    HomeView: Set DataContext and switch by CurrentView

--- NudelClient/Views/HomeView.xaml.cs
+++ NudelClient/Views/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nudel.Client.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Nudel.Client.Views
         public HomeView()
         {
             InitializeComponent();
+            DataContext = new HomeViewModel();
         }
     
 
