1599d4121cba36eb911574f45224ed5db2c56642
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-06-06 21:43:58 +0200

    Added JustConnect Project to Solution

---
+++ JustConnect/Tcp/Client.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace JustConnect.Tcp
+{
+    public delegate void ClientLogHandler(string data);
+    public delegate void ClientReceivedHandler(string data);
+
+    public class Client
+    {
+        private const int BUFFER_SIZE = 2048;
+        private Socket socket;
+        private byte[] buffer = new byte[BUFFER_SIZE];
+
+        public bool IsConnected { get; set; }
+        public int Port { get; set; }
+
+        public event ClientLogHandler Log;
+        public event ClientReceivedHandler Received;
+
+        public Client() : this(3131) { }
+        public Client(int port)
+        {
+            Port = port;
+        }
+
+        public void Connect(IPAddress ip)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            if (!IsConnected)
+            {
+                socket.Connect(ip, Port);
+                socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, Receive, socket);
+
+                IsConnected = true;
+
+                Log?.Invoke("Client connected");
+            }
+            else
+            {
+                Log?.Invoke("Client already connected");
+            }
+        }
+        public void Disconnect()
+        {
+            //socket.Disconnect(false);
+            socket.Close();
+
+            IsConnected = false;
+
+            Log?.Invoke("Client disconnected");
+        }
+        private void Receive(IAsyncResult AR)
+        {
+            Socket serverSocket = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = serverSocket.EndReceive(AR);
+
+                byte[] recBuf = new byte[received];
+                Array.Copy(buffer, recBuf, received);
+                string data = Encoding.ASCII.GetString(recBuf);
+
+                Received?.Invoke(data);
+
+                socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, Receive, socket);
+            }
+            catch (SocketException)
+            {
+                serverSocket.Close();
+
+                Log?.Invoke("Client disconnected");
+
+                return;
+            }
+        }
+        public void Send(String data)
+        {
+            new Thread(() =>
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+            }).Start();
+        }
+    }
+}
---
+++ JustConnect/Tcp/Server.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace JustConnect.Tcp
+{
+    public delegate void ServerLogHandler(string data);
+    public delegate void ServerReceivedHandler(string data, Socket client);
+    public delegate void ServerAcceptedHandler(Socket client);
+
+    public class Server
+    {
+        private const int BACKLOG = 10;
+        private const int BUFFER_SIZE = 2048;
+        private Socket socket;
+        private List<Socket> clients;
+        private byte[] buffer = new byte[BUFFER_SIZE];
+
+        public bool IsRunning { get; set; }
+        public int Port { get; set; }
+
+        public event ServerLogHandler Log;
+        public event ServerReceivedHandler Received;
+        public event ServerAcceptedHandler Accepted;
+
+        public Server() : this(3131) { }
+        public Server(int port)
+        {
+            Port = port;
+
+            clients = new List<Socket>();
+        }
+
+        public void Start()
+        {
+            if (!IsRunning)
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Bind(new IPEndPoint(IPAddress.Any, Port));
+                socket.Listen(BACKLOG);
+                socket.BeginAccept(Accept, null);
+
+                IsRunning = true;
+
+                Log?.Invoke("Server started");
+            }
+            else
+            {
+                Log?.Invoke("Server already running");
+            }
+        }
+        public void Stop()
+        {
+            foreach (Socket client in clients)
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+
+            socket.Close();
+            IsRunning = false;
+        }
+        private void Accept(IAsyncResult AR)
+        {
+            Socket clientSocket;
+
+            try
+            {
+                clientSocket = socket.EndAccept(AR);
+                clients.Add(clientSocket);
+
+                Log?.Invoke(((IPEndPoint)clientSocket.RemoteEndPoint).Address + " connected");
+
+                Accepted?.Invoke(clientSocket);
+
+                socket.BeginAccept(Accept, null);
+
+                clientSocket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, Receive, clientSocket);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+        private void Receive(IAsyncResult AR)
+        {
+            Socket clientSocket = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = clientSocket.EndReceive(AR);
+
+                byte[] recBuf = new byte[received];
+                Array.Copy(buffer, recBuf, received);
+                string data = Encoding.ASCII.GetString(recBuf);
+
+                Received?.Invoke(data, clientSocket);
+
+                clientSocket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, Receive, clientSocket);
+            }
+            catch (Exception e)
+            {
+                Log(e.ToString());
+
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                foreach (Socket client in clients)
+                {
+                    if (client == clientSocket)
+                    {
+                        Log?.Invoke(((IPEndPoint)client.RemoteEndPoint).Address + " disconnected");
+                        clients.Remove(client);
+                    }
+                }
+                clientSocket.Close();
+
+                return;
+            }
+        }
+        public void Send(string data, Socket clientSocket)
+        {
+            new Thread(() =>
+            {
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                clientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+            }).Start();
+
+        }
+        public void SendToAll(string data)
+        {
+            foreach (Socket client in clients)
+            {
+                Send(data, client);
+            }
+        }
+    }
+}
