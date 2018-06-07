947f323447ff9b45d8b6ba1918bf72fc7fb89bf9
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-06-07 18:59:24 +0200

    Implemented CreateRandomString

--- NudelBackend/NudelService.cs
+++ NudelBackend/NudelService.cs
@@ -149,5 +149,13 @@ namespace Nudel.Backend
 
             return null;
         }
+
+        private static string CreateRandomString(int length)
+        {
+            Random random = new Random();
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
     }
 }
