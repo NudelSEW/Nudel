42c44aee15a96b8bcdca9b51859ae385354d1d05
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-06-07 10:21:41 +0200

    Moved IsValidEmail function to Client Utils

---
+++ NudelClient/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nudel.Client
+{
+    public class Utils
+    {
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
