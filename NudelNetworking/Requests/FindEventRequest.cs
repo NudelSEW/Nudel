527b2302d5fb29e262aa641123adcb6e288d0a4e
Felician Joksch <fjoksch@student.tgm.ac.at>
2018-06-07 20:28:25 +0200

    Find Event Request

---
+++ NudelNetworking/Requests/FindEventRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nudel.Networking.Requests.Base
+{
+    class FindEventRequest : AuthenticatedRequest
+    {
+        public long ID { get; set; }
+      
+        public FindEventRequest() : base() { }
+
+        public FindEventRequest(string sessionToken, long id) : base(sessionToken)
+        {
+            ID = id;
+        }
+
+       
+
+    }
+}
