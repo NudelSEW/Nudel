97894de372ca0dcd6a87dce0a40d05308741bc51
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-06-07 09:10:06 +0200

    Location: Created operators

--- NudelBusinessObjects/Location.cs
+++ NudelBusinessObjects/Location.cs
@@ -10,4 +10,34 @@
             Latitude = latitude;
             Longitude = longitude;
         }
+
+        public static bool operator==(Location location1, Location location2) {
+            return location1.Equals(location2);
+        }
+
+        public static bool operator !=(Location location1, Location location2)
+        {
+            return !location1.Equals(location2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Location))
+            {
+                return false;
+            }
+
+            var location = (Location)obj;
+            return Latitude == location.Latitude &&
+                   Longitude == location.Longitude;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1416534245;
+            hashCode = hashCode * -1521134295 + Latitude.GetHashCode();
+            hashCode = hashCode * -1521134295 + Longitude.GetHashCode();
+            return hashCode;
+        }
+    }
 }
