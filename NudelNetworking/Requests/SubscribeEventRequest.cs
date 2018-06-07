5a9fe7a4bfbe3880b72fed4f6ea0606f0c39ae73
Felician Joksch <fjoksch@student.tgm.ac.at>
2018-06-07 20:44:46 +0200

    Subscribe Event Request

---
+++ NudelNetworking/Requests/SubscribeEventRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Nudel.Networking.Requests.Base;
+using Nudel.BusinessObjects;
+using System.Text;
+
+namespace Nudel.Networking.Requests
+{
+    class SubscribeEventRequest : AuthenticatedRequest
+    {
+        public long ID { get; set; }
+        public string Titel { get; set; }
+        public Event Event{ get; set; }
+        public SubscribeEventRequest() : base() { }
+
+        public SubscribeEventRequest(string sessionToken, long id) : base(sessionToken)
+        {
+            ID = id;
+        }
+
+        public SubscribeEventRequest(string sessionToken, string titel) : base(sessionToken)
+        {
+            Titel = titel;
+        }
+
+        public SubscribeEventRequest(string sessionToken, Event @event) : base(sessionToken)
+        {
+            Event = @event;
+        }
+    }
+}
