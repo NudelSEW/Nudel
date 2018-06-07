71354401471f401ce406d666e4675f3d47bd3e2b
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-06-07 09:34:47 +0200

    Location: changed to class and added Bson attributes

--- NudelBusinessObjects/Location.cs
+++ NudelBusinessObjects/Location.cs
@@ -1,8 +1,12 @@
-﻿namespace Nudel.BusinessObjects
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nudel.BusinessObjects
 {
-    public struct Location
+    public class Location
     {
+        [BsonElement("latitude")]
         public double Latitude { get; set; }
+        [BsonElement("longitude")]
         public double Longitude { get; set; }
 
         public Location(double latitude, double longitude)
