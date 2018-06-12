7aabc143ad5e526fd910e3a71d516a8b2bb3364a
Christoph Pader-Barosch <christoph.paderbarosch@gmail.com>
2018-06-12 23:50:59 +0200

    Fixed RegisterView Bugs

--- NudelClient/Views/RegisterView.xaml.cs
+++ NudelClient/Views/RegisterView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Nudel.Client.Model;
+using Nudel.Client.ViewModels;
+using Nudel.Networking.Requests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,48 @@ namespace Nudel.Client.Views
     /// </summary>
     public partial class RegisterView : UserControl
     {
+        public MainViewModel MainViewModel { get; set; }
+
         public RegisterView()
         {
             InitializeComponent();
+        }
+
+        private void RegisterView_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainViewModel = (MainViewModel)Window.GetWindow(this).DataContext;
+        }
+
+        private void Register(object sender, RoutedEventArgs e)
+        {
+            RegisterRequest request = new RegisterRequest
+            {
+                Username = username.Text,
+                Email = email.Text,
+                Password = password.Password,
+                FirstName = firstName.Text,
+                LastName = lastName.Text
+            };
+
+            ModelChangedHandler handler = null;
+            handler = (string fieldName, Object field) =>
+            {
+                if (fieldName == "SessionToken")
+                {
+                    MainViewModel.CurrentView = new HomeView();
+                }
+
+                MainModel.ModelChanged -= handler;
+            };
+
+            MainModel.ModelChanged += handler;
+
+            NetworkListener.SendRequest(request);
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            MainViewModel.CurrentView = new LoginView();
         }
     }
 }
